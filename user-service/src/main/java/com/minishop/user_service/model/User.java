30a39c3df721f0fe663225b9be36720bceaf6981
achrafmz <achrafmazouz50@gmail.com>
2025-07-17 15:32:12 +0100

    postgress configuration

--- user-service/src/main/java/com/minishop/user_service/model/User.java
+++ user-service/src/main/java/com/minishop/user_service/model/User.java
@@ -1,3 +1,4 @@
+// User.java
 package com.minishop.user_service.model;
 
 import jakarta.persistence.*;
@@ -13,6 +14,7 @@ public class User {
     private String username;
     private String password;
 
+    // Constructors
     public User() {}
 
     public User(String username, String password) {
@@ -20,5 +22,29 @@ public class User {
         this.password = password;
     }
 
-    // Getters et Setters
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
